deb64b0342d54e1f4acb4086a7bf5c6968b6c24d
Rodrigo Henrique Rocha <rodrigohr@correios.com.br>
2025-08-29 11:58:18 -0300

    aula 2 for e While

--- Aula2.py
+++ Aula2.py
@@ -1,17 +1,20 @@
 
-v = ('aeiouAEIOU')
-while True:
-    letra = input("Digite a letra que vc quer descobrir se é vogal ou consoante: ")
+# v = ('aeiouAEIOU')
+# while True:
+#     letra = input("Digite a letra que vc quer descobrir se é vogal ou consoante: ")
      
-    if len(letra)!= 1:
-        print("vc digitou mais de uma letra")
-        continue
+#     if len(letra)!= 1:
+#         print("vc digitou mais de uma letra")
+#         continue
            
-    for x in(letra):
-            if x in (v):
-                print("vc digitou uma vogal")
-            else:
-                print("vc digitou uma consoante")
-                break       
-    break  
+#     for x in(letra):
+#             if x in (v):
+#                 print("vc digitou uma vogal")
+#             else:
+#                 print("vc digitou uma consoante")
+#                 break       
+#     break  
+      
+b = ('a - e').strip()
+print(b)
         
